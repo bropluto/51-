6b0216c2e6dbea6cc909e4be9c0b389049b97aa0
Pluto <135139067+bropluto@users.noreply.github.com>
2023-06-28 00:10:24 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include <REGX52.H>
+#include <INTRINS.H>
+
+void Delay1000ms()		//@12.000MHz
+{
+	unsigned char data i, j, k;
+
+	_nop_();
+	i = 4;
+	j = 205;
+	k = 187;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
+}
 
 void main()
 {
-	
-	P2=0xfe;
+		while(1)
+		{
+			P2=0xfe;//ÁÁ
+			Delay1000ms();
+			P2=0xff;//Ãð 
+			Delay1000ms();
+		}
 }
